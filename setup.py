867039c89420dfc705001a861952af6757c37314
Akshat-Raj-Vansh <rajvanshakshat@gmail.com>
2025-07-19 16:53:30 +0530

    Created Packages using setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+HYPHEN_E_RE = '-e .'
+
 def get_requirements(file_path):
     """
     Reads the requirements from a file and returns a list of packages.
@@ -7,7 +9,8 @@ def get_requirements(file_path):
     with open(file_path, 'r') as file:
         requirements = file.readlines()
     # Remove any whitespace characters like `\n` at the end of each line
-    return [req.strip() for req in requirements if req.strip()]
+    requirements = [req.strip() for req in requirements if req.strip() and not req.startswith(HYPHEN_E_RE)]
+    return requirements
 
 setup(
     name='ml_project',
